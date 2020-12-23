272987894688c403e80c970fdbbc7551e7db3a8a
Tuan Duc Ngo <tdngo@TUANN-MAC.mgm-edv.de>
2020-12-23 10:10:08 +0700

    implement upload feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React from "react";
+
 import "./App.css";
 import { imageUploader as ImageUploader } from "./components/image-uploader/image-uploader.js";
 
-function App() {
-	return (
-		<div className="App">
-			<ImageUploader status="upload" />
-		</div>
-	);
-}
+//https://images.yourdomain.com/photo-1496950866446-325
 
-export default App;
+export class App extends React.Component {
+	constructor(props) {
+		super();
+
+		this.state = {
+			status: "upload",
+			link: "",
+		};
+
+		this.uploadFileHandler = this.uploadFileHandler.bind(this);
+	}
+
+	uploadFileHandler(item, status) {
+		console.log(item, status);
+		setTimeout(() => {
+			this.setState({
+				status: status,
+				link: `https://images.mydomain.com/${item[0].name
+					.split(" ")
+					.join("-")}`,
+			});
+		}, 3000);
+
+		this.setState({ status: "loading" });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<ImageUploader
+					status={this.state.status}
+					onChangeHandler={this.uploadFileHandler}
+					link={this.state.link}
+				/>
+			</div>
+		);
+	}
+}
--- src/components/image-uploader/image-uploader.js
+++ src/components/image-uploader/image-uploader.js
@@ -5,78 +5,134 @@ import "./image-uploader.css";
 import previewImage from "../../images/preview-image.svg";
 import image from "../../images/image.png";
 
-export const imageUploader = (props) => {
-	return (
-		<div className="image-uploader">
-			<div className="image-uploader__container">
-				<div className="image-uploader__header">
-					{props.status === "upload" && (
-						<>
-							<div className="image-uploader__title">Upload Your Image</div>
-							<div className="image-uploader__description">
-								File should be jpeg, png,...
+export class imageUploader extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.uploadInput = React.createRef();
+		this.focusUploadInputHandler = this.focusUploadInputHandler.bind(this);
+		this.fileUploadHanlder = this.fileUploadHanlder.bind(this);
+		this.onChangeFileHandler = this.onChangeFileHandler.bind(this);
+		this.onDropHandler = this.onDropHandler.bind(this);
+		this.onDragOverHandler = this.onDragOverHandler.bind(this);
+	}
+
+	fileUploadHanlder(file, status) {
+		if (this.props.onChangeHandler) {
+			this.props.onChangeHandler(file, status);
+		}
+	}
+
+	onChangeFileHandler(e) {
+		console.log(e.target.files);
+		if (e.target.files && e.target.files.length > 0) {
+			this.fileUploadHanlder(e.target.files, "success");
+		}
+	}
+
+	focusUploadInputHandler() {
+		this.uploadInput.current.click();
+	}
+
+	onDragOverHandler(e) {
+		e.preventDefault();
+	}
+
+	onDropHandler(e) {
+		e.preventDefault();
+		this.fileUploadHanlder(e.dataTransfer.files, "success");
+	}
+
+	render() {
+		return (
+			<div className="image-uploader">
+				<div className="image-uploader__container">
+					<div className="image-uploader__header">
+						{this.props.status === "upload" && (
+							<>
+								<div className="image-uploader__title">
+									Upload Your Image
+								</div>
+								<div className="image-uploader__description">
+									File should be jpeg, png,...
+								</div>
+							</>
+						)}
+						{this.props.status === "success" && (
+							<>
+								<Icon color="green" name="check circle" />
+								<div className="image-uploader__description">
+									Uploaded Successfully!
+								</div>
+							</>
+						)}
+					</div>
+					<div className="image-uploader__body">
+						{this.props.status === "loading" && (
+							<div className="image-uploader__loading">
+								<Segment>
+									<Loader active>Uploading...</Loader>
+								</Segment>
 							</div>
-						</>
-					)}
-					{props.status === "success" && (
-						<>
-							<Icon color="green" name="check circle" />
-							<div className="image-uploader__description">
-								Uploaded Successfully!
+						)}
+						{this.props.status === "upload" && (
+							<div
+								className="image-uploader__dnd-area"
+								onDrop={this.onDropHandler}
+								onDragOver={this.onDragOverHandler}
+							>
+								<img
+									src={previewImage}
+									className="image-uploader__dnd-preview-image"
+									alt="preview"
+								/>
+								<div className="image-uploader__dnd-label">
+									Drag and Drop Your Image Here
+								</div>
+								<input
+									type="file"
+									className="image-uploader__input"
+									onChange={this.onChangeFileHandler}
+									ref={this.uploadInput}
+								></input>
 							</div>
-						</>
-					)}
-				</div>
-				<div className="image-uploader__body">
-					{props.status === "loading" && (
-						<div className="image-uploader__loading">
-							<Segment>
-								<Loader active>Uploading...</Loader>
-							</Segment>
-						</div>
-					)}
-					{props.status === "upload" && (
-						<div className="image-uploader__dnd-area">
-							<img
-								src={previewImage}
-								className="image-uploader__dnd-preview-image"
-								alt="preview"
-							/>
-							<div className="image-uploader__dnd-label">
-								Drag and Drop Your Image Here
+						)}
+						{this.props.status === "success" && (
+							<div className="image-uploader__uploaded-area">
+								<img
+									className="image-uploader__uploaded-image"
+									src={image}
+									alt="success"
+								/>
 							</div>
-							<input type="file" className="image-uploader__input"></input>
-						</div>
-					)}
-					{props.status === "success" && (
-						<div className="image-uploader__uploaded-area">
-							<img
-								className="image-uploader__uploaded-image"
-								src={image}
-								alt="success"
-							/>
-						</div>
-					)}
-				</div>
-				<div className="image-uploader__footer">
-					{props.status === "upload" && (
-						<>
-							<p>OR</p>
-							<Button primary>Choose a file</Button>
-						</>
-					)}
-					{props.status === "success" && (
-						<div className="image-uploader__share-area">
-							<div className="image-uploader__link">
-								https://images.yourdomain.com/photo-1496950866446-325
+						)}
+					</div>
+					<div className="image-uploader__footer">
+						{this.props.status === "upload" && (
+							<>
+								<p>OR</p>
+
+								<Button
+									primary
+									onClick={this.focusUploadInputHandler}
+								>
+									Choose a file
+								</Button>
+							</>
+						)}
+						{this.props.status === "success" && (
+							<div className="image-uploader__share-area">
+								<div className="image-uploader__link">
+									{this.props.link}
+								</div>
+								<Button primary size="large">
+									Copy Link
+								</Button>
 							</div>
-							<Button primary size="large">
-								Copy Link
-							</Button>
-						</div>
-					)}
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
