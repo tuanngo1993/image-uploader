a5488b8651b9bb505b161e29d76db2541b857e9a
Tuan Ngo <tuannaru1993@gmail.com>
2020-12-24 10:33:18 +0700

    implement upload feature (#1)
    * implement upload feature
    
    * create image prop
    
    * delete console.log & update name of class based compoenent
    
    * implement the copy link feature
    
    * split File Upload component up into small components and update a few style
    
    * deloy in IE11
    
    Co-authored-by: Tuan Duc Ngo <tdngo@TUANN-MAC.mgm-edv.de>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React from "react";
+
 import "./App.css";
-import { imageUploader as ImageUploader } from "./components/image-uploader/image-uploader.js";
 
-function App() {
-	return (
-		<div className="App">
-			<ImageUploader status="upload" />
-		</div>
-	);
+import { ImageUploader } from "./components/image-uploader/image-uploader.js";
+import { PreviewUploader } from "./components/image-uploader/preview-uploader.js";
+import { LoadingUploader } from "./components/image-uploader/loading-uploader.js";
+
+export class App extends React.Component {
+	constructor(props) {
+		super();
+
+		this.state = {
+			status: "upload",
+			url: "",
+		};
+
+		this.handleFileUpload = this.handleFileUpload.bind(this);
+	}
+
+	handleFileUpload(item) {
+		setTimeout(() => {
+			this.setState({
+				status: "success",
+				url: process.env.PUBLIC_URL + "/images/image.png",
+			});
+		}, 3000);
+
+		this.setState({ status: "loading" });
+	}
+
+	render() {
+		const { status, url } = this.state;
+		return (
+			<div className="App">
+				{status === "upload" && (
+					<PreviewUploader onChange={this.handleFileUpload} />
+				)}
+				{status === "loading" && <LoadingUploader />}
+				{status === "success" && <ImageUploader url={url} />}
+			</div>
+		);
+	}
 }
-
-export default App;
--- src/components/image-uploader/image-uploader.js
+++ src/components/image-uploader/image-uploader.js
@@ -1,80 +1,43 @@
 import React from "react";
-import { Button, Loader, Segment, Icon } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 import "./image-uploader.css";
-import previewImage from "../../images/preview-image.svg";
-import image from "../../images/image.png";
 
-export const imageUploader = (props) => {
+export const ImageUploader = (props) => {
+	const handleCopyLink = () => {
+		navigator.clipboard
+			.writeText(props.url)
+			.then(() => {
+				alert("Copied link!!!");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<div className="image-uploader">
 			<div className="image-uploader__container">
 				<div className="image-uploader__header">
-					{props.status === "upload" && (
-						<>
-							<div className="image-uploader__title">Upload Your Image</div>
-							<div className="image-uploader__description">
-								File should be jpeg, png,...
-							</div>
-						</>
-					)}
-					{props.status === "success" && (
-						<>
-							<Icon color="green" name="check circle" />
-							<div className="image-uploader__description">
-								Uploaded Successfully!
-							</div>
-						</>
-					)}
+					<Icon color="green" name="check circle" />
+					<div className="image-uploader__description">
+						Uploaded Successfully!
+					</div>
 				</div>
 				<div className="image-uploader__body">
-					{props.status === "loading" && (
-						<div className="image-uploader__loading">
-							<Segment>
-								<Loader active>Uploading...</Loader>
-							</Segment>
+					<div className="image-uploader__uploaded-area">
+						<div className="image-uploader__uploaded-image">
+							<img src={props.url} alt="success" />
 						</div>
-					)}
-					{props.status === "upload" && (
-						<div className="image-uploader__dnd-area">
-							<img
-								src={previewImage}
-								className="image-uploader__dnd-preview-image"
-								alt="preview"
-							/>
-							<div className="image-uploader__dnd-label">
-								Drag and Drop Your Image Here
-							</div>
-							<input type="file" className="image-uploader__input"></input>
-						</div>
-					)}
-					{props.status === "success" && (
-						<div className="image-uploader__uploaded-area">
-							<img
-								className="image-uploader__uploaded-image"
-								src={image}
-								alt="success"
-							/>
-						</div>
-					)}
+					</div>
 				</div>
 				<div className="image-uploader__footer">
-					{props.status === "upload" && (
-						<>
-							<p>OR</p>
-							<Button primary>Choose a file</Button>
-						</>
-					)}
-					{props.status === "success" && (
-						<div className="image-uploader__share-area">
-							<div className="image-uploader__link">
-								https://images.yourdomain.com/photo-1496950866446-325
-							</div>
-							<Button primary size="large">
-								Copy Link
-							</Button>
-						</div>
-					)}
+					<div className="image-uploader__share-area">
+						<div className="image-uploader__link">{props.url}</div>
+						<Button primary size="large" onClick={handleCopyLink}>
+							Copy Link
+						</Button>
+					</div>
 				</div>
 			</div>
 		</div>
